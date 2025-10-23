8ebf5de3a0bb9a8acb2bceb1b946ddae94757d08
daveycodez <daveycodez@gmail.com>
2025-10-23 00:25:27 -0700

    Add staticParams for prerendering post IDs
    Introduced a staticParams function to specify which postIds should be prerendered for the /posts/$postId route. This enables static generation for selected posts.

--- src/routes/posts/$postId.tsx
+++ src/routes/posts/$postId.tsx
@@ -1,5 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router"
 
+// Add this to specify which postIds to prerender
+export const staticParams = () => {
+    return [
+        { postId: "1" },
+        { postId: "2" },
+        { postId: "77" }
+        // Add more IDs you want to prerender
+    ]
+}
+
 export const Route = createFileRoute("/posts/$postId")({
     component: RouteComponent
 })

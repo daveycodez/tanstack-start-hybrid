dce90d41e6a89e7e58c737d06a06fab357069ccb
daveycodez <daveycodez@gmail.com>
2025-10-25 17:50:01 -0700

    Add request logging to fetch handler
    Added a console.log statement to log incoming requests in the fetch handler for debugging purposes.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import handler from "@tanstack/react-start/server-entry"
 
 export default {
     fetch(request: Request) {
+        console.log("request", request)
         // const url = new URL(request.url)
         // if (url.pathname === "/test") {
         //     return Response.redirect(`${url.origin}/`, 302)
